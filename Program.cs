3c27d0b73a8b9cd7bc4d7359338dd6dbfb05f514
Diamond Mccall <singlikediamond@gmail.com>
2023-02-25 14:02:41 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fizzbuzz {
+
+class Program  {
+
+public static void Main(); {
+
+public string Fizzbuzz(int number)
+{
+    var answer = "";
+
+    if (number % 3 == 0)
+    {
+        answer = "fizz";
+    }
+    if (number % 5 == 0)
+    {
+        answer = "buzz"
+    
+    }
+    else
+    {
+        answer = "fizzbuzz"
+    }
+}   
+}
+}
+}
+
+
